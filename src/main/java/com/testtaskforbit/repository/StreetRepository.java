35f2f12d7011fe6755aee68566a96064a8714369
Hikanon <roma.makhanov@mail.ru>
2023-04-11 18:22:51 +0300

    Add repositories

--- src/main/java/com/testtaskforbit/repository/StreetRepository.java
+++ src/main/java/com/testtaskforbit/repository/StreetRepository.java
@@ -4,6 +4,11 @@ import com.testtaskforbit.entity.Street;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StreetRepository extends JpaRepository<Street, Integer> {
+
+    List<Street> findStreetsByCityId(Integer cityId);
+
 }
